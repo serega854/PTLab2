8893a66e3abae3c4819d170e9c108653847af91f
Serega854 <novruzovsergey2002@yandex.ru>
2024-12-21 09:17:49 +0300

    сейчас кол-во меняется, цена не меняется, добавил столбец исходное количество

--- shop/models.py
+++ shop/models.py
@@ -4,9 +4,11 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
+    quantity_beg = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name  
+
 
 class Purchase(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
--- shop/views.py
+++ shop/views.py
@@ -1,38 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
 from .models import Product, Purchase
+
 
 def index(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'shop/index.html', context)
 
+
 class PurchaseCreate(CreateView):
     model = Purchase
-    fields = ['person', 'address']  # Убираем product из полей формы, чтобы не было путаницы
-
+    fields = ['person', 'address']
+    
     def get_initial(self):
         initial = super().get_initial()
-        initial['product'] = Product.objects.get(pk=self.kwargs['product_id'])
+        try:
+            product = get_object_or_404(Product, pk=self.kwargs['product_id'])
+            initial['product'] = product
+        except Exception as e:
+            print(e)  # Логирование ошибки
+        
         return initial
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
-        product = Product.objects.get(pk=self.kwargs['product_id'])
+        product = get_object_or_404(Product, pk=self.kwargs['product_id'])
+
+        # Получаем начальное количество из базы данных
         initial_quantity = product.quantity
-
-        if initial_quantity > 0:
+        
+        if product.quantity > 0:
+            previous_quantity = product.quantity
             product.quantity -= 1
-            
-            # Увеличиваем цену на 20%, если количество уменьшилось более чем в два раза
-            if product.quantity < (initial_quantity / 2):
-                product.price *= 1.2
-            
-            product.save()
-            self.object.product = product  # Присваиваем продукт к покупке
-            self.object.save()
 
+            # Проверяем, уменьшилось ли количество более чем в два раза
+            if previous_quantity > 0 and product.quantity <= (initial_quantity // 2):  # Условие деления пополам
+                product.price *= 0.8  # Уменьшаем цену на 20%
+                print(f"Цена товара {product.name} изменена на {product.price}")
+
+            product.save()  # Сохранение изменений количества и цены
+            self.object.product = product
+            self.object.save()
+            
             return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
         else:
             return HttpResponse('Извините, товар закончился.')
